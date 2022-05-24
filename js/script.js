eb40f5c4d3377da5ce3205f9b3fbf72c45326b3f
Matias Velazquez <matiasrv@live.com>
2022-05-24 00:24:32 -0300

    keyboard compatibility

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const display = document.querySelector('#display');
 const operators = document.getElementById("operators");
+let operatorsDisabled = false;
 const clear = document.getElementById("clear")
 const buttonC = document.getElementById("c")
 const digits = document.getElementById("numbers");
@@ -7,10 +8,10 @@ const zero = document.getElementById('zero');
 const equal = document.getElementById("equal");
 const dot = document.getElementById("dot");
 equal.disabled = true;
-const DISPLAYOVERFLOW = 25;
+const DISPLAYOVERFLOW = 23;
 let n1, n2, operator;
 n1 = 0;
-let divisionError = "CAN'T DIVIDE BY ZERO";
+let unexpectedError = "UNEXPECTED ERROR";
 
 function add(){
     return +n1 + +n2;
@@ -20,7 +21,7 @@ function substract(){
 }
 function divide(){
     let result;
-    (n2 == 0) ? result = divisionError : result = +n1 / +n2;
+    (n2 == 0) ? result = unexpectedError : result = +n1 / +n2;
     return result;
 }
 function multiply(){
@@ -39,18 +40,42 @@ function operate(){
             break;
         default:
             n1 = display.textContent = divide(n1,n2).toString();
-            if(n1 == divisionError){
-                n1 = 0;
-            }
             break;
+    }
+    if(!isFinite(n1)){
+        display.textContent = unexpectedError;
+        n1 = 0;
     }
     operator = n2 = undefined;
 }
 function addDigit(e){
     if(display.textContent.length > DISPLAYOVERFLOW){return};
-    if(e == '0'){
+    if(isFinite(e) && e != 0){
         if(operator == undefined){
-            if(n1 != 0){
+            n1 == '0' ? display.textContent = e: display.textContent += e;
+            n1 == 0 ? n1 = e : n1 += e;         
+        }
+        else{
+            if (n2 == '0'){
+                n2 = e;
+                display.textContent = display.textContent.slice(0,-1) + e;
+            }
+            else if(n2 != undefined){
+                n2 += e;
+                display.textContent += e;
+            }
+            else{
+                n2 = e;
+                display.textContent += e;
+            }
+            equal.disabled = false;
+            operators.childNodes.forEach(button => button.disabled = false);
+            operatorsDisabled = false;
+        }
+    }
+    else if(e == '0'){
+        if(operator == undefined){
+            if(n1 != '0'){
                 display.textContent += zero.textContent;
                 n1 += zero.textContent.toString();
             }        
@@ -66,43 +91,22 @@ function addDigit(e){
             }
             equal.disabled = false;
             operators.childNodes.forEach(button => button.disabled = false);
-        }
-    }else if(e.type == 'click'){
-        if(e.target.tagName == "BUTTON"){
-            if(operator == undefined){
-                n1 == '0' ? display.textContent = e.target.textContent: display.textContent += e.target.textContent;
-                n1 == 0 ? n1 = e.target.textContent.toString(): n1 += e.target.textContent.toString();         
-            }
-            else{
-                if (n2 == '0'){
-                    n2 = e.target.textContent.toString();
-                    display.textContent = display.textContent.slice(0,-1) + e.target.textContent;
-                }
-                else if(n2 != undefined){
-                    n2 += e.target.textContent.toString();
-                    display.textContent += e.target.textContent;
-                }
-                else{
-                    n2 = e.target.textContent.toString();
-                    display.textContent += e.target.textContent;
-                }
-                equal.disabled = false;
-                operators.childNodes.forEach(button => button.disabled = false);
-            }
+            operatorsDisabled = false;
         }
     }
 }
-digits.addEventListener("click", addDigit);
-zero.addEventListener("click", addDigit('0'));
+digits.addEventListener("click", e => addDigit(e.target.textContent));
+zero.addEventListener("click", ()=> addDigit('0'));
 clear.addEventListener("click", ()=>{
     n1 = display.textContent = 0;
     n2 = operator = undefined;
     equal.disabled = true;
     operators.childNodes.forEach(button => button.disabled = false);
+    operatorsDisabled = false;
     dot.disabled = false;
 });
 function backSpace(){
-    if(display.textContent.length > 1 && display.textContent != divisionError){
+    if(display.textContent.length > 1 && display.textContent != unexpectedError){
         display.textContent = display.textContent.slice(0, -1);
     }else{
         display.textContent = '0';
@@ -116,11 +120,13 @@ function backSpace(){
             n2 = undefined;
             equal.disabled = true;
             operators.childNodes.forEach(button => button.disabled = true);
+            operatorsDisabled = true;
         }
     }
     else if(operator != undefined && n2 == undefined){
         operator = undefined;
         operators.childNodes.forEach(button => button.disabled = false);
+        operatorsDisabled = false;
     }
     else if(n1.length >= 1){
         if(n1.slice(-1, n1.length) == '.'){
@@ -134,20 +140,22 @@ function backSpace(){
 }
 buttonC.addEventListener("click", backSpace);
 function addOperator(e){
-    if(e.target.tagName == "BUTTON"){
-        if(n2 != undefined){
-            operate();
-        }
-        display.textContent += e.target.textContent;
-        operator = e.target.textContent;
-        operators.childNodes.forEach(b => b.disabled = true);
-        dot.disabled = false;
+    if(operatorsDisabled){return};
+    if(n2 != undefined){
+        operate();
     }
+    display.textContent += e;
+    operator = e;
+    operators.childNodes.forEach(b => b.disabled = true);
+    operatorsDisabled = true;
+    dot.disabled = false;
 }
-operators.addEventListener("click", addOperator);
+operators.addEventListener("click", e => addOperator(e.target.textContent));
 function evaluateEqual(){
+    if(n2 == undefined){return};
     operate();
     operators.childNodes.forEach(button => button.disabled = false);
+    operatorsDisabled = false;
     equal.disabled = true;
     dot.disabled = false;
 };
@@ -169,3 +177,35 @@ function addDot(){
     dot.disabled = true;
 };
 dot.addEventListener("click", addDot);
+
+document.body.addEventListener("keydown", k =>{
+    switch(k.key){
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+        case '0':
+            addDigit(k.key);
+            break;
+        case 'Backspace':
+            backSpace();
+            break;
+        case '/':
+        case '*':
+        case '-':
+        case '+':
+            addOperator(k.key);
+            break;
+        case "Enter":
+            evaluateEqual();
+            break;
+        case '.':
+            if(!dot.disabled){addDot()};
+            break;
+    }
+})
